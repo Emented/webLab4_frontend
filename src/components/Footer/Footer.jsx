42550e0f3854a5f8b6c4646ba5755fafcc7441ff
Ilya <ilerys61@gmail.com>
2023-01-03 16:03:20 +0600

    Small style fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,14 +7,14 @@ const gitUrl = "https://github.com/Emented";
 
 const Footer = () => {
 
-    const gitClickAction = () => window.location = gitUrl;
+    const gitClickAction = () => window.open(gitUrl);
 
     return (
         <footer className="footer">
             <div className="footer-text">
                 <h2 className="footer-text-name">Povetin Ilya</h2>
             </div>
-            <GitHubIcon onClick={gitClickAction} fontSize="large">GitHub</GitHubIcon>
+            <GitHubIcon cursor="pointer" onClick={gitClickAction} fontSize="large">GitHub</GitHubIcon>
         </footer>
     );
 
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,8 +7,8 @@ const NotFound = (props) => {
     return (
         <div className="not-found-wrapper">
             <div className="tile">
-                    <h1 className="not-found-number">404</h1>
-                    <h2 className="not-found-text">Page not found</h2>
+                <h1 className="not-found-number">404</h1>
+                <h2 className="not-found-text">Page not found</h2>
             </div>
         </div>
     );
