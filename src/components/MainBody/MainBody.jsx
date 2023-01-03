518a4169383e4b3a4e9af08880c02a547aedd089
Ilya <ilerys61@gmail.com>
2023-01-03 18:59:05 +0600

    Add work with hits mobiles

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -4,12 +4,13 @@ import React from 'react';
 import Graph from "./components/Graph/Graph";
 import CoordinatesForm from "./components/CoordinatesForm/CoordinatesForm";
 import DataTable from "./components/DataTable/DataTable";
+import isMobile from "../../service/isMobile";
 
 const MainBody = (props) => {
 
     return (
         <div className="main-body">
-            <div className="main-body-inputs">
+            <div className={isMobile() ? 'main-body-inputs-mobile' : 'main-body-inputs'}>
                 <Graph/>
                 <CoordinatesForm/>
             </div>
